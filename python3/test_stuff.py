30fbd3aa20c9617b8d7297506c56c11d97a25e39
Jonathan Koch <devthat@mailbox.org>
2018-04-22 19:59:45 +0200

    Next shot

--- python3/test_stuff.py
+++ python3/test_stuff.py
@@ -3,6 +3,6 @@ import unittest
 
 class SomeTest(unittest.TestCase):
     def test_it(self):
-        print "this is some test"
+        print("this is some test")
 
         self.assertEqual(True, 1 == 1)
